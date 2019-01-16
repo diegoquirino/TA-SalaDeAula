f1640c8eb0da58dac2145ca5a920270e1f0f6c34
Carlos Diego Quirino Lima <diegoquirino@gmail.com>
2019-01-16 00:30:16 -0300

    Projeto Base para Selenide

--- src/test/java/br/pro/diegoquirino/calculadora/ux/EyesSingleton.java
+++ src/test/java/br/pro/diegoquirino/calculadora/ux/EyesSingleton.java
@@ -1,14 +1,8 @@
 package br.pro.diegoquirino.calculadora.ux;
 
-import com.applitools.eyes.RectangleSize;
 import com.applitools.eyes.exceptions.DiffsFoundException;
 import com.applitools.eyes.selenium.Eyes;
 import net.thucydides.core.webdriver.WebDriverFacade;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.awt.*;
 
 public class EyesSingleton {
 
@@ -25,7 +19,7 @@ public class EyesSingleton {
             eyes = new Eyes();
             // Change or Externalize a APIKEY para a da sua conta, a fim de que seja possível gerenciar mudanças
             // e planos de execução
-            eyes.setApiKey("4m5H5pKs4QqslbtPMUcsqdIe59CSXGgdq1541Pp60qM110");
+            eyes.setApiKey("<<SUA APIKEY>>");
         }
         return eyes;
     }
